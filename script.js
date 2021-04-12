f738412aee706bee47eae7a51dd5955ecb7d4754
MSebb23 <matteosabbatini23@gmail.com>
2021-04-12 02:08:06 +0200

    Show data into albums
    linked html and API with Vue

--- script.js
+++ script.js
@@ -4,10 +4,42 @@ var app_Vue = new Vue (
     {
         el : "#app_Vue",
         data : {
-            album : [],
+            album_arr : []
         },
         mounted : function () {
-            axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((risposta) => {
+
+            // console.log(this, self);  //this e self sono la stessa cosa  
+            axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((answer) => {
+
+                // console.log(answer.data.response);
+                this.album_arr = answer.data.response;
+
+                
+                // console.log(answer.data.response[0].poster);
+
+            });
+        }
+
+    }
+);
+
+/*
+* trash code:
+mounted :  function() {
+                console.log(this.email_list);
+                var self = this;
+                for(var i = 0; i != email_numbers; i++) {
+                    console.log(i);    
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(function (answer) {
+    
+                        // console.log(self.email_list);
+                        self.email = answer.data.response;
+                        self.email_list.push(self.email);
+                        // self.email_list.push(self.email)
+                    })
+                }
+
+axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((risposta) => {
 
                 // console.log(risposta);
                 // console.log(risposta.data);
@@ -17,7 +49,18 @@ var app_Vue = new Vue (
                 console.log(this.album[0].genre);
 
             })
-        }
 
-    }
-)
+this.album_arr.forEach((element, index )  =>
+
+                    // self.poster.push(element[index]),
+                    console.log( answer.data.response[index].poster),
+                    
+                    // !! perchè index funziona solo nel console.log ?
+                    // value = answer.data.response[index].poster,
+                    // self.poster_arr.push(value),
+                    // console.log(poster_arr, value)
+
+                    // if (this.genres)
+
+                );
+*/
